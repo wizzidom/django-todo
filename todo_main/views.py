114410851d31cc99b9495d42b9657642b12ca443
wizzidom <gwativengawisdom@gmail.com>
2025-07-30 13:53:08 +0200

    delete fucntionaliity

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,8 +5,13 @@ from todos.models import Todo
 
 def home(request): 
     tasks = Todo.objects.filter(task_completed = False).order_by('-updated_at')
+    completed = Todo.objects.filter(task_completed = True)
     print(tasks)
+    print(completed)
     context = {
-        'tasks' : tasks
+        'tasks' : tasks,
+        'completed' : completed
     }
     return render(request, 'home.html', context)
+
+
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 
 from todos.models import Todo
 
@@ -10,6 +10,31 @@ def addTask(request):
     task = request.POST['task']
     Todo.objects.create(task = task)
     return redirect('home')
+
+# def completedTask(request):
+#     if request.method == "POST":
+#         task_id = request.POST.get('task_id')
+#         if task_id:
+#             try:
+#                 todo = Todo.objects.get(id=task_id)
+#                 todo.task_completed = True
+#                 todo.save()
+#             except Todo.DoesNotExist:
+#                 pass
+#     return redirect('home')
+    
+def completedTask(request, pk):
+    task = get_object_or_404(Todo, pk = pk)
+    task.task_completed = True
+    task.save()
+    print(task.task_completed)
+    return redirect('home')
+
+def delete(request, pk):
+    task = get_object_or_404(Todo, pk = pk)
+    task.delete()
+    return redirect('home')
+   
 
 
 
